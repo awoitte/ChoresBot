073e5572b3db30be7a55680aab4bf1f80252e5a0
Alicia Woitte <aliciawoitte@gmail.com>
2021-10-10 10:53:08 -0700

    Initial Commit

---
+++ client/src/index.ts
@@ -0,0 +1,9 @@
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = "Hello World"
+
+    return element;
+}
+
+document.body.appendChild(component());
